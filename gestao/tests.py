a37ef378020cbed2924ee3a01c528f5a6b452086
André "decko" de Brito <brito.afa@gmail.com>
2018-01-08 17:53:02 -0200

    Confirma erro através de teste
    Confirma o erro #80 através de um teste.

--- gestao/tests.py
+++ gestao/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from model_mommy import mommy
+
+from gestao.views import diligencia_documental
+
+
+@pytest.mark.django_db
+def test_diligencia_documental(client):
+
+    plano_cultura = mommy.make('PlanoCultura')
+    plano_trabalho = mommy.make('PlanoTrabalho', plano_cultura=plano_cultura)
+    user = mommy.make('Usuario', plano_trabalho=plano_trabalho)
+    etapa = 'plano_cultura'
+    st = 'situacao_lei_plano_id'
+
+    request = client.get('/gestao/diligencia/{}/{}/{}'.format(
+        etapa,
+        st,
+        user.id))
+
+    with pytest.raises(AttributeError) as exception:
+        diligencia_documental(request, etapa, st, user.id)
+
+    # __import__('ipdb').set_trace()
+    assert 'AttributeError' == exception.typename
