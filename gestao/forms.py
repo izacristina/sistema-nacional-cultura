316c9978667623496959089085c9c7636ee5d8ec
André "decko" de Brito <brito.afa@gmail.com>
2018-03-27 14:50:58 -0300

    [FIX] Altera a referencia à situação de cada componente.

--- gestao/forms.py
+++ gestao/forms.py
@@ -137,11 +137,11 @@ class AlterarSituacao(ModelForm):
                 orgao_gestor = OrgaoGestor()
                 plano_cultura = PlanoCultura()
 
-                conselho_cultural.situacao_ata_id = 0
-                criacao_sistema.situacao_lei_sistema_id = 0
-                fundo_cultura.situacao_lei_plano_id = 0
-                orgao_gestor.situacao_relatorio_secretaria_id = 0
-                plano_cultura.situacao_lei_plano_id = 0
+                conselho_cultural.situacao.id = 0
+                criacao_sistema.situacao.id = 0
+                fundo_cultura.situacao.id = 0
+                orgao_gestor.situacao.id = 0
+                plano_cultura.situacao.id = 0
 
                 if commit:
                     criacao_sistema.save()
